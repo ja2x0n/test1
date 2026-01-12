4cdf70db69ae210bb1d8b3a095e3811f2df27f9e
jae1 <t01046993182@gmail.com>
2026-01-12 12:22:25 +0900

    first commit

---
+++ src/main/java/com/example/test/controller/CustomerController.java
@@ -0,0 +1,20 @@
+package com.example.test.controller;
+
+import com.example.test.repository.CustomerRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Repository;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RequiredArgsConstructor
+@Controller
+public class CustomerController {
+    private final CustomerRepository customerRepository;
+
+    @GetMapping("/")
+    public String getCustomer(Model model){
+        model.addAttribute("customers",customerRepository.findAll());
+        return "customers";
+    }
+}
